60cca032503b47bbe5267389ab008d38204ef32c
lemonclown <chaosillu@gmail.com>
2021-07-12 20:23:12 +0900

    Hide bottom tab bar on ContentsModal screen

--- app/showbility/mainTab.js
+++ app/showbility/mainTab.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import ShowbilityHome from './components/shobilityHome';
 import SearchTab from './components/search';
 import NewUploadTab from './components/newUpload';
@@ -8,12 +9,18 @@ import MyShowbilTab from './components/myShowbil';
 
 const MainTab = createBottomTabNavigator();
 
+const getVisibility = (route) => {
+    const routeName = getFocusedRouteNameFromRoute(route);
+    if (routeName === "ContentsModal") {
+        return false;
+    }
+    return true;
+}
+
 function MainTabScreen() {
     return (
       <MainTab.Navigator>
-        <MainTab.Screen name="쇼빌리티" component={ShowbilityHome}
-          // tabBarVisible={visible}
-          />
+        <MainTab.Screen name="쇼빌리티" component={ShowbilityHome} options={({route}) => ({ tabBarVisible: getVisibility(route) })}/>
         <MainTab.Screen name="검색" component={SearchTab} />
         <MainTab.Screen name="NewUpload" component={NewUploadTab} />
         <MainTab.Screen name="메세지" component={MessageTab} />
