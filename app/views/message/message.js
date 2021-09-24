915d029de9ecb6e2a8d4b704a9c3e4e0170d234a
lemonclown <chaosillu@gmail.com>
2021-09-24 17:35:07 +0900

    Fixed message flatlist item view

--- app/views/message/message.js
+++ app/views/message/message.js
@@ -1,5 +1,12 @@
 import * as React from 'react';
-import {View, Text, StyleSheet, SafeAreaView, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  FlatList,
+} from 'react-native';
 import {TextInput} from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
@@ -21,6 +28,55 @@ const styles = StyleSheet.create({
 });
 
 function MessageTab() {
+  let messageData = [
+    {
+      sender: 'Hyechou',
+      latestMessage: '너무 멋진 작품입니다.',
+      latestMessageTime: '14:23',
+    },
+    {
+      sender: 'Hyechou2',
+      latestMessage: '너무 멋진 작품입니다.',
+      latestMessageTime: '14:23',
+    },
+  ];
+
+  const renderItem = object => {
+    return (
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{flex: 1, maxWidth: 62, padding: 7, paddingLeft: 0}}>
+          <Image
+            style={{width: null, height: null, aspectRatio: 1, flex: 1}}
+            resizeMode={'contain'}
+            source={require('../../../assets/imgs/add_image.png')}
+          />
+        </View>
+        <View style={{flex: 1}}>
+          <View style={{flex: 1, flexDirection: 'row', paddingTop: 5}}>
+            <View style={{flex: 1}}>
+              <Text style={{fontSize: 17}}>Hyechou</Text>
+            </View>
+            <View
+              style={{
+                alignItems: 'flex-end',
+                flex: 1,
+              }}>
+              <View style={{flexDirection: 'row'}}>
+                <Text style={{flex: 1, maxWidth: 50, color: '#B2B2B5'}}>
+                  14:23
+                </Text>
+                <Text style={{flex: 1, maxWidth: 10}}>{'>'}</Text>
+              </View>
+            </View>
+          </View>
+          <View style={{flex: 1}}>
+            <Text style={{fontSize: 12}}>너무 멋진 작품입니다.</Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView style={[styles.flexCenter, styles.container]}>
       <View style={{paddingHorizontal: 15, flex: 1, paddingTop: 30}}>
@@ -40,30 +96,15 @@ function MessageTab() {
         <View style={{flex: 1, maxHeight: 50}}>
           <TextInput placeholder="검색" style={styles.searchStyle} />
         </View>
-        {/* <FlatList /> */}
         <View style={{flex: 1}}>
-          <View style={{flex: 1, flexDirection: 'row', maxHeight: 62}}>
-            <View style={{flex: 1, maxWidth: 62}}>
-              <Text>Image</Text>
-            </View>
-            <View style={{flex: 1}}>
-              <View style={{flex: 1, flexDirection: 'row'}}>
-                <View style={{flex: 1}}>
-                  <Text>name</Text>
-                </View>
-                <View
-                  style={{
-                    alignItems: 'flex-end',
-                    flex: 1,
-                  }}>
-                  <Text>{'>'}</Text>
-                </View>
-              </View>
-              <View>
-                <Text>recentmessage</Text>
-              </View>
-            </View>
-          </View>
+          <FlatList
+            key={'#'}
+            keyExtractor={item => '#' + item.sender}
+            data={messageData}
+            renderItem={renderItem}
+            horizontal={false}
+            style={{flex: 1}}
+          />
         </View>
       </View>
     </SafeAreaView>
