0df0260279e7d1f45be03d29bdac9fc59caefab6
lemonclown <chaosillu@gmail.com>
2021-10-18 02:23:46 +0900

    Get categories and map to search view

--- app/views/components/search.js
+++ app/views/components/search.js
@@ -9,6 +9,7 @@ import {
   FlatList,
   TouchableOpacity,
 } from 'react-native';
+import {getCategoryList} from '../../service/ability';
 
 const styles = StyleSheet.create({
   container: {
@@ -65,58 +66,20 @@ const styles = StyleSheet.create({
 });
 
 function SearchTab() {
-  let data = [
-    {
-      id: 0,
-      url: 'https://i.pinimg.com/564x/08/94/75/089475365c284288406baf7e5616dd64.jpg',
-      name: '포토그래피',
-    },
-    {
-      id: 1,
-      url: 'https://i.pinimg.com/236x/4b/ee/eb/4beeebb760923f65d559e3486f1233c1.jpg',
-      name: '일러스트레이션',
-    },
-    {
-      id: 2,
-      url: 'https://i.pinimg.com/564x/b7/a5/a8/b7a5a801d8b9476bad5906ad88347445.jpg',
-      name: '건축',
-    },
-    {
-      id: 3,
-      url: 'https://i.pinimg.com/564x/08/94/75/089475365c284288406baf7e5616dd64.jpg',
-      name: '포토그래피',
-    },
-    {
-      id: 4,
-      url: 'https://i.pinimg.com/236x/4b/ee/eb/4beeebb760923f65d559e3486f1233c1.jpg',
-      name: '일러스트레이션',
-    },
-    {
-      id: 5,
-      url: 'https://i.pinimg.com/564x/b7/a5/a8/b7a5a801d8b9476bad5906ad88347445.jpg',
-      name: '건축',
-    },
-    {
-      id: 6,
-      url: 'https://i.pinimg.com/564x/08/94/75/089475365c284288406baf7e5616dd64.jpg',
-      name: '포토그래피',
-    },
-    {
-      id: 7,
-      url: 'https://i.pinimg.com/236x/4b/ee/eb/4beeebb760923f65d559e3486f1233c1.jpg',
-      name: '일러스트레이션',
-    },
-    {
-      id: 8,
-      url: 'https://i.pinimg.com/564x/b7/a5/a8/b7a5a801d8b9476bad5906ad88347445.jpg',
-      name: '건축',
-    },
-  ];
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    getCategoryList().then(res => {
+      console.log(res);
+      setData(res.results);
+    });
+  }, []);
+
   const renderItem = itemObject => {
     let item = itemObject.item;
     return (
       <TouchableOpacity style={styles.abilityFrame}>
-        <Image source={{uri: item.url}} style={styles.flatListImage} />
+        <Image source={{uri: item.image}} style={styles.flatListImage} />
         <Text style={[styles.fontJeju, styles.abilityItemTitle]}>
           {item.name}
         </Text>
