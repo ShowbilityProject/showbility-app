5c57d64c45388e1d5fd3d3f9c88a925d9fa05123
lemonclown <chaosillu@gmail.com>
2021-10-30 15:45:40 +0900

    Update ios project name

--- ios/ShowbilityTests/ShowbilityTests.m
+++ ios/ShowbilityTests/ShowbilityTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface Sample2Tests : XCTestCase
+@interface ShowbilityTests : XCTestCase
 
 @end
 
-@implementation Sample2Tests
+@implementation ShowbilityTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
