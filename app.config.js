1026b49deb706c5c2ad7134ad74ee1c8f06f3352
Soonho Kwon <percy3368@gmail.com>
2023-08-02 23:20:32 +0900

    Modify app version to match submitted version

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default () => ({
     name: "쇼빌리티",
     slug: "showbility",
     owner: "showbility",
-    version: "1.0.0",
+    version: "1.2.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
