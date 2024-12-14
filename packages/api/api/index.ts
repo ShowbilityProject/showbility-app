70bbe2f2180859dc00ade54d91655dfe1686fe15
Soonho Kwon <percy3368@gmail.com>
2024-12-14 18:29:35 +0900

    tsconfig-paths

--- packages/api/api/index.ts
+++ packages/api/api/index.ts
@@ -1,3 +1,11 @@
+import { compilerOptions } from "../tsconfig.json";
+import { register } from "tsconfig-paths";
+
+register({
+  baseUrl: compilerOptions.baseUrl,
+  paths: compilerOptions.paths,
+});
+
 import { app } from "@/app.js";
 import { handle } from "hono/vercel";
 
