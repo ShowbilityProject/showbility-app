fad3b70de3e58a961379af9d78186d453d270c42
Soonho Kwon <percy3368@gmail.com>
2024-12-16 12:24:55 +0900

    add auto migration

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
-import { env } from "./src/env.js";
+import { env } from "./src/env";
 
 export default defineConfig({
   out: "./migrations",
