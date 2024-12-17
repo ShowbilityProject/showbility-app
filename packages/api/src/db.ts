87184e1fb1c3f1633bff9dd9484f35ac384da475
Soonho Kwon <percy3368@gmail.com>
2024-12-18 02:14:59 +0900

    setup drizzle connection

--- packages/api/src/db.ts
+++ packages/api/src/db.ts
@@ -1,10 +1,9 @@
 import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
 import { env } from "./env.js";
 
-const client = createClient({
-  url: env.DB_URL,
-  authToken: env.DB_AUTH_TOKEN,
+export const db = drizzle({
+  connection: {
+    url: env.DB_URL,
+    authToken: env.DB_AUTH_TOKEN,
+  },
 });
-
-export const db = drizzle(client);
