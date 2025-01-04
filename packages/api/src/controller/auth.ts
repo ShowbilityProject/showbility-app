ccca83fe1125dfac9c7011b7b317724eed9795ab
Soonho Kwon <percy3368@gmail.com>
2025-01-04 23:01:21 +0900

    remove email constraint

--- packages/api/src/controller/auth.ts
+++ packages/api/src/controller/auth.ts
@@ -15,10 +15,8 @@ type AuthResponse =
   | { isRegistered: false; registerToken: string; prefill: { name?: string } };
 
 const authController = new Hono()
-  .post(
-    "/test",
-    zValidator("json", z.object({ email: z.string().email() })),
-    c => c.json({ message: "test" }),
+  .post("/test", zValidator("json", z.object({ email: z.string() })), c =>
+    c.json({ message: "test" }),
   )
   .post(
     "/email/send",
