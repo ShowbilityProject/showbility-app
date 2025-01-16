bb5a5956693fa1e44ee04932bc7c508d05301081
Soonho Kwon <percy3368@gmail.com>
2025-01-16 21:47:14 +0900

    disable bodyparser

--- packages/api/api/index.ts
+++ packages/api/api/index.ts
@@ -1,4 +1,10 @@
 import { handle } from "@hono/node-server/vercel";
 import { app } from "../src/app.js";
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default handle(app);
