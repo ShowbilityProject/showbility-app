2ab1e8437b9dc2b17d3d01453fa74a63084ffade
Soonho Kwon <percy3368@gmail.com>
2025-01-30 03:38:17 +0900

    Rollback to single package

--- packages/api/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { app } from "./app.js";
-
-export type AppType = typeof app;
--- packages/api/src/utils/assert.ts
+++
@@ -1,8 +0,0 @@
-export const assert = (
-  condition: unknown,
-  error?: Error | string,
-): asserts condition => {
-  if (!condition) {
-    throw typeof error === "string" ? new Error(error) : error;
-  }
-};
